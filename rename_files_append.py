0b862a5e2c8e6b12e2b692a4f034be5118f5a00a
Sinisa Koscec <sinisa@dvasadva.com>
2023-04-05 03:53:27 +0200

    bs4

---
+++ rename_files_append.py
@@ -0,0 +1,17 @@
+import os
+
+base_folder = './folders'
+folder_paths = [f for f in os.listdir(base_folder) if not f.startswith('.')]
+
+for i in folder_paths:
+
+    folder_path = os.path.join(base_folder, i)
+
+    for filename in [f for f in os.listdir(folder_path) if not f.startswith('.')]:
+        base_filename = os.path.splitext(filename)[0]
+        file_extension = os.path.splitext(filename)[1]
+
+        new_base_filename = '-' + base_filename
+        new_filename = i + new_base_filename + file_extension
+        os.rename(os.path.join(folder_path, filename),
+                  os.path.join(folder_path, new_filename))
