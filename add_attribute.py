7101ccf97781f7a3f569e030459e067afbc0f17c
Sinisa Koscec <sinisa@dvasadva.com>
2023-09-07 14:24:23 +0200

    sizechart filters

--- add_attribute.py
+++ add_attribute.py
@@ -21,10 +21,34 @@ new_attribute = {
     'position': 5,
     'visible': True,
     'variation': False,
-    'options': ['SAUMFTW']
+    'options': ['SAUMAPP']
 }
 
-product_skus = ['S70665-3', 'S70665-14']
+product_skus = [
+    'SAM800301-BKH',
+    'SAM800275-BK',
+    'SAM800244-LN',
+    'SAM800190-BK',
+    'SAM800243-BK',
+    'SAM800279-LGH',
+    'SAM800278-RNH',
+    'SAM800278-LGH',
+    'SA81165-FL',
+    'SAM800277-RNH',
+    'SAM800296-BK',
+    'SAM800299-BK',
+    'SAM800297-BK',
+    'SAM800310-BK',
+    'SAM800185-DGHBK',
+    'SAM800273-DGH',
+    'SAM800303-BKH',
+    'SAM800286-BK',
+    'SAM800212-SL',
+    'SAM800112-LM',
+    'SAM800039-DGH',
+    'SA81283-VPPR',
+    'SA81392-BLBVPP']
+
 for product_sku in product_skus:
     product = wc_api.get('products', params={'sku': product_sku}).json()[0]
     existing_attributes = product["attributes"]
